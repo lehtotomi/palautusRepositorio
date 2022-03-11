c7bc8e818b37620314ce2bbbc61b4834ea89a239
Tomi Lehto <tomi.lehto@hotmail.fi>
2022-03-11 19:16:42 +0200

    t1.3 changes

--- osa1/t1.1/src/App.js
+++ osa1/t1.1/src/App.js
@@ -10,6 +10,7 @@ const Header = (props) => {
       <h1>{props.course}</h1>
   )
 }
+
 const Part = (props) => {
   return (
     <p>
@@ -19,7 +20,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  return(
+  return (
     <>
     <Part part={props.part1} exercise={props.exercise1}/>
     <Part part={props.part2} exercise={props.exercise2}/>
@@ -38,19 +39,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2}
-       part3={part3} exercise3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1.name} exercise1={part1.exercises}
+        part2={part2.name} exercise2={part2.exercises}
+        part3={part3.name} exercise3={part3.exercises}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
