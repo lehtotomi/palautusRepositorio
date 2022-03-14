a890632fbb2ef58f6c22ab119fdd54b9e66bbc47
Tomi Lehto <tomi.lehto@hotmail.fi>
2022-03-14 17:44:20 +0200

    2.5 changes

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,55 +1,7 @@
-const Header = ( {name} ) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Part = ( { part, exercise } ) => {
-  return (
-    <p>
-      {part} {exercise}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(
-        value => 
-          <Part key={value.id}
-                part={value.name}
-                exercise={value.exercises}
-          />
-      )}
-    </>
-  )
-}
 
 
-const Total = ( {parts} ) => {
-  return (
-    <>
-      {parts.reduce(
-        (summa, value) => summa + value.exercises, 0
-      )}
-    </>
-  )
-}
 
-
-const Course = ( {course} ) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <b>
-        Total number of exercises: <Total parts={course.parts} />
-      </b>
-    </>
-  )
-}
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,59 @@
+
+
+const Header = ( {name} ) => {
+    return (
+      <h1>{name}</h1>
+    )
+}
+  
+  
+const Part = ( { part, exercise } ) => {
+    return (
+      <p>
+        {part} {exercise}
+      </p>
+    )
+}
+  
+  
+const Content = ({ parts }) => {
+    return (
+      <>
+        {parts.map(
+          value => 
+            <Part key={value.id}
+                  part={value.name}
+                  exercise={value.exercises}
+            />
+        )}
+      </>
+    )
+}
+  
+  
+  
+const Total = ( {parts} ) => {
+    return (
+      <>
+        {parts.reduce(
+          (summa, value) => summa + value.exercises, 0
+        )}
+      </>
+    )
+}
+  
+  
+const Course = ( {course} ) => {
+    return (
+        <>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <b>
+                Total number of exercises: <Total parts={course.parts} />
+            </b>
+        </>
+    )
+}
+
+export default Course
+        
